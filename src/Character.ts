f754c8d69f4f2df2500b4e5bea6c0115221d7646
Leila Moraes <leila.violino@gmail.com>
2023-03-25 18:46:31 -0300

    fix: type de energy na interface Fighter

---
+++ src/Character.ts
@@ -0,0 +1,48 @@
+import Archetype from './Archetypes';
+import Energy from './Energy';
+import Fighter from './Fighter';
+import Race from './Races';
+import getRandomInt from './utils';
+
+export default class Character implements Fighter {
+  private _race: Race;
+  private _archetype: Archetype;
+  private maxLifePoints: number;
+  private _lifePoints: number;
+  private _strength: number;
+  private _defense: number;
+  private _dexterity: number;
+  private _energy: Energy;
+
+  constructor(private _name: string) {
+    this._dexterity = getRandomInt(1, 10);
+  }
+
+  get race(): Race {
+    return this._race;
+  }
+
+  get archetype(): Archetype {
+    return this._archetype;
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  get defense(): number {
+    return this._defense;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
+  }
+
+  get energy(): Energy {
+    return this._energy;
+  }
+}
